120dcd21810fe7228f01dfc063ae5bf8a39b41ef
christie <christiecarolbeauchamp@gmail.com>
2021-01-09 13:26:51 +0000

    Wrote a jobs page.

---
+++ nexus/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+        colors: {
+            'terminal-purple': '#2d0922',
+            'command-green': '#7cfd38',
+            'command-blue': '#669be2',
+            'white': '#FFFFFF',
+        },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
